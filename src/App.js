a609e069e52a74eb303f3dfe15c6844c82ed2d56
LEARN Academy <learnacademy@LEARNs-MacBook-Air-3.local>
2022-05-31 11:57:30 -0700

    completed up to adding the tree

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["🗺", "🗺", "🗺", "🗺", "🗺", "🗺", "🗺", "🗺", "🗺"]
     }
+  }
+  handleGamePlay = (index) => {
+    const { board } = this.state
+    board[index] = "🌳"
+    this.setState({board: board})
   }
 
   render() {
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div className= "game-board">
+          {this.state.board.map((value, index) => {
+            return (
+              <Square
+                key={index}
+                value={value}
+                index={index}
+                handleGamePlay={this.handleGamePlay}
+
+              />
+            )
+          })}
+        </div>
       </>
     )
   }
